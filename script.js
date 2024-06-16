1c8e5e7f97cd75cbfaf40a9d3ef13ba6343a898b
TheMasher3 <61765821+TheMasher3@users.noreply.github.com>
2024-06-16 16:16:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,57 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const albums = {
+const songsToAlbums = {
+        "Girl With The Tattoo Enter.lewd": "all_i_want_is_you",
+        "Lost": "channel_orange",
+        "Good News": "circles",
+        "LOVE. FEAT. ZACARI.": "damn",
+        "See You Again (feat. Kali Uchis)": "flower_boy"
+};
+
+const albums = {
         //optional: replace with the id you used in html, then replace the information
         //Copy and paste move if needed
+        all_i_want_is_you: {
+            title: 'All I Want Is You',
+            artist: 'Miguel',
+            releaseDate: 'November 30, 2010',
+            genre: 'R&B',
+            description: 'All I Want Is You is the debut studio album by American singer Miguel.',
+            color: 'rgb(176, 37, 40, 0.8)'
+        },
+        channel_orange: {
+            title: 'channel ORANGE',
+            artist: 'Frank Ocean',
+            releaseDate: 'July 10, 2012',
+            genre: 'R&B, neo soul',
+            description: 'channel ORANGE is the debut studio album by American R&B singer Frank Ocean.',
+            color: 'rgba(243, 117, 33, 0.8)'
+        },
+        circles: {
+            title: 'Circles',
+            artist: 'Mac Miller',
+            releaseDate: 'January 17, 2020',
+            genre: 'Hip hop, soul',
+            description: 'Circles is the sixth and final studio album by American rapper and singer Mac Miller.',
+            color: 'rgba(255, 255, 255, 0.8)'
+        },
+        damn: {
+            title: 'DAMN.',
+            artist: 'Kendrick Lamar',
+            releaseDate: 'April 14, 2017',
+            genre: 'Hip hop',
+            description: 'DAMN. is the fourth studio album by American rapper Kendrick Lamar.',
+            color: 'rgb(217, 28, 30, 0.8)'
+        },
         flower_boy: {
             title: 'Flower Boy',
             artist: 'Tyler, the Creator',
             releaseDate: 'July 21, 2017',
             genre: 'Hip hop, neo soul',
-            description: 'Flower Boy is the fourth studio album by American rapper Tyler, the Creator.'
-        },
-        hollywoods_bleeding: {
-            title: 'Hollywood\'s Bleeding',
-            artist: 'Post Malone',
-            releaseDate: 'September 6, 2019',
-            genre: 'Hip hop, pop, trap',
-            description: 'Hollywood\'s Bleeding is the third studio album by American rapper Post Malone.'
-        },
-        swimming: {
-            title: 'Swimming',
-            artist: 'Mac Miller',
-            releaseDate: 'August 3, 2018',
-            genre: 'Hip hop, jazz rap',
-            description: 'Swimming is the fifth studio album by American rapper and singer Mac Miller.'
-        },
-        all_eyez_on_me: {
-            title: 'All Eyez on Me',
-            artist: '2Pac',
-            releaseDate: 'February 13, 1996',
-            genre: 'Hip hop',
-            description: 'All Eyez on Me is the fourth studio album by American rapper 2Pac.'
-        },
-        miss_education: {
-            title: 'The Miseducation of Lauryn Hill',
-            artist: 'Lauryn Hill',
-            releaseDate: 'August 25, 1998',
-            genre: 'R&B, hip hop, soul',
-            description: 'The Miseducation of Lauryn Hill is the debut solo album by American singer and rapper Lauryn Hill.'
-        },
-        songs_about_jane: {
-            title: 'Songs About Jane',
-            artist: 'Maroon 5',
-            releaseDate: 'June 25, 2002',
-            genre: 'Pop rock, funk rock',
-            description: 'Songs About Jane is the debut studio album by American pop rock band Maroon 5.'
+            description: 'Flower Boy is the fourth studio album by American rapper Tyler, the Creator.',
+            color: 'rgba(252, 175, 38, 0.8)'
         }
     };
+document.addEventListener('DOMContentLoaded', function() {
+
 
     Object.keys(albums).forEach(id => {
         document.getElementById(id)?.addEventListener('click', function(event) {
@@ -64,47 +71,66 @@ document.addEventListener('DOMContentLoaded', function() {
         `;
 
         // Position the pop-up
-        albumInfoDiv.style.display = 'block';  // Make the pop-up visible
         const imgElement = event.target;
         const imgRect = imgElement.getBoundingClientRect();
-        const popUpWidth = albumInfoDiv.offsetWidth;
-        const popUpHeight = albumInfoDiv.offsetHeight;
+        const infoDivRect = albumInfoDiv.getBoundingClientRect();
 
-        let top = imgRect.top + window.scrollY;
-        let left = imgRect.right + 10;
+        const viewportWidth = window.innerWidth;
+        const viewportHeight = window.innerHeight;
 
-        // Adjust if pop-up would go outside the right edge of the screen
-        if (left + popUpWidth > window.innerWidth) {
-            left = imgRect.left - popUpWidth - 10;
+        let left = imgRect.right + 10; // Default position to the right of the image
+        let top = imgRect.top;
+
+        if (left + infoDivRect.width > viewportWidth) {
+            left = imgRect.left - infoDivRect.width - 10; // Position to the left if it overflows
         }
 
-        // Adjust if pop-up would go outside the bottom edge of the screen
-        if (top + popUpHeight > window.innerHeight + window.scrollY) {
-            top = window.innerHeight + window.scrollY - popUpHeight - 10;
+        if (top + infoDivRect.height > viewportHeight) {
+            top = viewportHeight - infoDivRect.height - 10; // Adjust to prevent bottom overflow
         }
 
-        // Make sure it doesn't go outside the left edge
-        if (left < 0) {
-            left = 10; // Add some padding
-        }
-
-        // Make sure it doesn't go outside the top edge
-        if (top < window.scrollY) {
-            top = window.scrollY + 10; // Add some padding
-        }
-
-        albumInfoDiv.style.top = `${top}px`;
         albumInfoDiv.style.left = `${left}px`;
+        albumInfoDiv.style.top = `${top}px`;
+
+        albumInfoDiv.style.display = 'block';
         albumInfoDiv.style.opacity = 1;
     }
 
-    const themeToggle = document.getElementById('theme-toggle');
-    themeToggle.addEventListener('change', () => {
-        document.body.classList.toggle('dark-mode', themeToggle.checked);
+    document.getElementById('theme-toggle').addEventListener('change', () => {
+        document.body.classList.toggle('dark-mode');
     });
 });
 
 function closePopup() {
     const albumInfoDiv = document.getElementById('album-info');
     albumInfoDiv.style.display = 'none'; // Hide the pop-up
+}
+
+function livelyCurrentTrack(data) {
+    let obj = JSON.parse(data);
+    if (obj != null) {
+        const songTitle = obj.Title;
+        const albumIds = songsToAlbums[songTitle];
+
+        // Remove glow from all albums
+        document.querySelectorAll('.glow').forEach(element => {
+            element.classList.remove('glow');
+            element.style.boxShadow = '';
+        });
+
+        if (albumIds) {
+            // If albumIds is a string, convert it to an array for uniform processing
+            const albumIdArray = Array.isArray(albumIds) ? albumIds : [albumIds];
+
+            // Add glow to the current albums
+            albumIdArray.forEach(albumId => {
+                const albumElement = document.getElementById(albumId);
+                if (albumElement) {
+                    const albumColor = albums[albumId].color;
+                    albumElement.classList.add('glow');
+                    albumElement.style.boxShadow = `0 0 20px 5px ${albumColor}`;
+                }
+            });
+        }
+    }
 }
